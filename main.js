f9b6f1b925572202c8a293b6a62d04e6dfd48f8a
ngayMaivanden <datlai304@gmail.com>
2023-10-20 16:40:01 +0700

    'handle-onclick'

--- main.js
+++ main.js
@@ -23,51 +23,92 @@ const awaitDelay = (ms) =>
 
 let count = 0;
 let pos = 0;
+let flag = false;
+let myActive;
 const autoPlay = () => {
   clearInterval(timerAutoPlay);
   timerAutoPlay = setInterval(async () => {
-    // for(let index = 0; index < listELementImage.length; index++) {
-    //     listELementImage[index].style.opacity = '.5'
-    // }
-    // if(goal == 0) {
-    //  pos = pos + -155;
+console.log('bug', goal)
+    if (!flag) {
+  await awaitDelay(1000)
 
-    //       bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
-    //       sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
-    //     // listELementImage[goal].style.opacity = 1
-    // goal++
-    //       console.log("pos", pos);
+      for (let index = 0; index < sliderItem; index++) {
+        if (sliderItem[index].classList.contains("flag")) {
+          sliderItem[index].classList.remove("flag");
+        }
+      }
 
-    // }
-    if (goal > listELementImage.length - 1) {
-      sliderWrapper.style.display = "none";
+      if (goal > listELementImage.length - 1) {
+        sliderWrapper.style.display = "none";
+        sliderWrapper.style.opacity = 0;
+        sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
+        await awaitDelay(2000);
+        sliderWrapper.style.opacity = 1;
+        goal = 0;
 
-      sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
-      await awaitDelay(500);
-      goal = 0;
+        bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
 
-      bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
+        pos = 0;
+        console.log("pos", pos);
+      } else {
+        pos = pos + -100;
+        // console.log("goal", goal);
+        listELementImage[goal].style.opacity = 1;
+        sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
+        sliderItem[goal].classList.add("active");
+        sliderItem[goal].classList.add("flag");
+        myActive = goal;
+        sliderWrapper.style.display = "flex";
 
+        // sliderItem[goal].style.boxShadow =
+        //   "rgba(255, 255, 255, 0.8) 0px 0px 7px 8px";
+        bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
+        await awaitDelay(1200);
+        listELementImage[goal].style.opacity = ".5";
+        sliderItem[goal].classList.remove("active");
 
-      pos = 0;
-      console.log("pos", pos);
+        goal++;
+      }
     } else {
-      pos = pos + -185;
-      console.log("goal", goal);
-      listELementImage[goal].style.opacity = 1;
-      sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
-      sliderWrapper.style.display = "flex";
 
-      sliderItem[goal].style.boxShadow =
-        "rgba(255, 255, 255, 0.8) 0px 0px 7px 8px";
-      bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
-      await awaitDelay(1200);
-      listELementImage[goal].style.opacity = ".5";
-      sliderItem[goal].style.boxShadow = "none";
-
-      goal++;
-    }
+  await awaitDelay(1500)
+}
   }, 1500);
+};
+
+const getPositionGoalElementActive = async (a) => {
+ 
+
+  let elementClickData = Number(a.dataset.active);
+  let eleFlag = document.querySelectorAll(".slider-item")[goal];
+  eleFlag.classList.remove("active");
+  goal = elementClickData;
+  sliderItem[goal].classList.add("active");
+  let elementPrevData = Number(eleFlag.dataset.active);
+  flag = true;
+
+  if (elementClickData < elementPrevData) {
+    let chenhLech =   elementClickData - elementPrevData;
+    // console.log(chenhLech)
+    pos = pos - 100 * -1;
+
+    sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
+    bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
+  } else {
+    let chenhLech = elementPrevData - elementClickData;
+    // console.log(elementClickData, elementPrevData);
+    pos = pos + 100 * chenhLech;
+    sliderContainer.style.transform = `translate3d(0px, ${pos}px,0px)`;
+    bossImage.style.backgroundImage = `url(${listELementImage[goal].src})`;
+  // console.log("debug", chenhLech);
+  }
+  // console.log(eleFlag.dataset.active)
+
+  // let topElement = a.getBoundingClientRect().top
+  await awaitDelay(1500)
+  flag = false;
+
+  // console.log('global check', flag)
 };
 
 autoPlay();
